e1ea08ba57d6cf4302c27018f125f853261e5698
Jung Yunseo <yunseo2488@naver.com>
2025-09-12 14:12:56 +0900

    loading.. 출력

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,34 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <div>여기는 /posts 페이지입니다.</div>;
+  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      });
+  }, []);
+
+  return (
+    
+    <div className="flex flex-col gap-9">
+      <h1>글 목록</h1>
+      // 아무것도 없으면 Loading... 출력
+      {posts.length === 0 && <div>Loading...</div>}
+      {posts.length > 0 && (
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.id} : {post.title}
+          </li>
+        ))}
+      </ul>
+      )}
+    </div>
+  );
 }
